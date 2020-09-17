b8dd67579032eec973cd639fc366cbb8e35d4e3b
Mari <charmee2928@gmail.com>
2020-09-18 01:41:05 +0900

    Update and rename edges.txt to findCommonChars.java

---
+++ String/findCommonChars.java
@@ -0,0 +1,27 @@
+static int gemstones(String[] arr) {
+
+    ArrayList<Character> bag = new ArrayList<>();
+    ArrayList<Character> bag2 = new ArrayList<>();
+    int count = 0;
+    for(int i = 0; i < arr[0].length(); i++){
+        bag.add(arr[0].charAt(i));
+    }
+    // start comparing
+    for(int i = 1; i < arr.length; i++){
+        String s = arr[i];
+        for(int a = 0; a < s.length(); a++){
+            if(bag.contains(s.charAt(a))){
+                bag2.add(s.charAt(a));
+                int pos = bag.indexOf(s.charAt(a));
+                bag.remove(pos);
+            }                
+        }
+        bag.clear();
+        for(int a = 0; a < bag2.size(); a++){
+            bag.add(bag2.get(a));
+        }            
+        bag2.clear();          
+    }
+    return bag.size();
+
+}
