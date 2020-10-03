4c10ae145c7d0e8fdf737be86a1b97ca37df1c92
Mari <charmee2928@gmail.com>
2020-10-03 18:39:57 +0900

    Create matrix.java

---
+++ Graph/matrix.java
@@ -0,0 +1,53 @@
+/*
+    Grouping nodes and compute the number of combination of a pair of 2.
+*/
+
+static boolean[] visited;
+static ArrayList<Integer> path = new ArrayList<>();
+
+static long journeyToMoon(int n, int[][] astronaut){
+
+    ArrayList<ArrayList<Integer>> all = new ArrayList<>();
+    visited = new boolean[n];
+    int countN = 0;
+    
+    // create a matrix graph
+    int[][] g = new int[n][n];
+    for(int i =0; i < astronaut.length; i++){
+        int x = astronaut[i][0];
+        int y = astronaut[i][1];
+        g[x][y] = 1;
+        g[y][x] = 1;
+    }
+    
+    // DFS for grouping
+    for(int i =0; i < n; i++){
+        if(!visited[i]){
+            path.clear();
+            DFS(i, g);                
+            countN += path.size();
+            all.add((ArrayList<Integer>)path.clone());
+        }
+    }
+    
+    // count the number of pairs
+    long count = 0;
+    for(int j = 0; j < all.size(); j++){
+        ArrayList<Integer> each = all.get(j);
+        for(int i = j+1; i < all.size(); i++){
+            ArrayList<Integer> each2 = all.get(i);
+            count += each.size() * each2.size();
+        }
+    }       
+    return count; 
+}
+
+static void DFS(int root, int[][] g){
+    path.add(root);
+    visited[root] = true;
+    for(int i = 0; i <g.length; i++){
+        if(g[root][i] == 1 && visited[i] == false){
+            DFS(i, g);
+        }
+    }
+}
