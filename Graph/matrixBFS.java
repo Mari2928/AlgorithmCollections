e88da37544acdde1a0a471a61d19d0e9c579a1c9
Mari <charmee2928@gmail.com>
2021-01-14 16:24:29 +0900

    Update matrixBFS.java

--- Graph/matrixBFS.java
+++ Graph/matrixBFS.java
@@ -1,90 +1,77 @@
-/*
-    Create a matrix graph and BFS to find shortest distances from s to each node (6 per edge).
-    4 2     -> the number of nodes and edges
-    [1 2]
-    [1 3]
-    1       -> start node
-    Output: [6 6 -1] -> 1 to 2, 1 to 3, and 1 to 4
-*/
+import java.io.*;
+import java.util.*;
 
-static int[] bfs(int n, int m, int[][] edges, int s) {
-    int[][] g = createGraph(n, m, edges);
+public class Solution {
 
-    // BFS for traversing 
-    int[] result = new int[n+1];                 // edge counts
-    Arrays.fill(result, -1);
-    ArrayList<Integer> path = new ArrayList<>(); // BFS path
-    Queue<Integer> q = new ArrayDeque<>();       // visiting list
-    boolean[] visited = new boolean[g.length+1]; // visiting status
-
-    q.add(s);
-    visited[s] = true;
-    
-    while(!q.isEmpty()){
-        int t = q.peek(); 
-        path.add(t);      
-        // count the number of edges
-        if(t != s) {
-            result[t] = countEdges(g, t, path);
-        }            
-        // get adjacents
-        for(int i = 1; i < g.length; i++){
-            if(g[t][i] == 1 && !visited[i]){ 
-                q.add(i);  
-                visited[i] = true;
+    public static void main(String[] args) {
+        /* Shortest path from a start node.
+            
+            Input:
+            2   -> q: number of queries
+            4 2 -> n, m: number of nodes and edges
+            1 2 -> u, v: edge connection
+            1 3
+            1   -> s: start node
+            3 1
+            2 3
+            2
+            
+            Output:
+            6 6 -1
+            -1 6
+         */
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        for(int j = 0; j < q; j++){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] g = new int[n+1][n+1]; // create a graph
+            for(int i = 0; i < m; i++){
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+                g[u][v] = 1;
+                g[v][u] = 1;
             }
-        }            
-        q.remove();            
+            int s = sc.nextInt();
+            ArrayList<Integer> distances = shortestDistFromS(s, n, g);
+            for(Integer val : distances)
+                System.out.print(val +" ");
+            System.out.println();
+        }        
     }
-    return formatResult(n, s, result);
-}
-
-/*
-    Create a matrix graph.
-*/
-static int[][] createGraph(int n, int m, int[][] edges){        
-    int[][] g = new int[n+1][n+1];
-    for(int i =0; i < m; i++){
-        int x = edges[i][0];
-        int y = edges[i][1];
-        g[x][y] = 1;
-        g[y][x] = 1;
+    /* Get a list of shortest distances from start to each node */
+    static ArrayList<Integer> shortestDistFromS(int start, int n, int[][] g){
+        ArrayList<Integer> distances = new ArrayList<>();
+        for(int goal = 1; goal <= n; goal++){
+            if(goal != start)
+                distances.add(BFS(start, goal, g, n));  
+        }    
+        return distances;    
     }
-    return g;
-}
-
-/*
-    Format the resulted counting list as required.
-*/
-static int[] formatResult(int n, int start, int[] result){ 
-    int[] result2 = new int[n-1];
-    int j = 0;
-    for(int i = 1; i <= n; i++){
-        if(i == start)  continue;   // remove a start node
-        result2[j] = result[i];
-        j++;
-    }
-    return result2;
-}
-
-/*
-    Count edges to find a shortest distance on each destination.
-*/
-static int countEdges(int[][] g, int t, ArrayList<Integer> path){
-    int count = 0;
-    int start = path.get(0);
-
-    // start from t and move backward toward start
-    while(t != start){
-        int current = start;
-        int i = 0;
-        // if edge is found, increment the count
-        while(g[t][current] != 1){ 
-            i++;
-            current = path.get(i); // start node moves forward
+    
+    /* BFS to get a shortest distance from start to goal */
+    static int BFS(int start, int goal, int[][] g, int n){
+        boolean[] visited = new boolean[n+1];
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        q.add(start);
+        visited[start] = true;
+        dist[start] = 0;
+        while(!q.isEmpty()){
+            int t = q.peek();            
+            if(t == goal && t != start) 
+                return dist[goal];
+            for(int i = 1; i < g.length; i++){
+                if(g[t][i]==1 && !visited[i]){
+                    q.add(i);
+                    visited[i] = true;
+                    dist[i] = dist[t]+6;
+                }
+            }
+            q.remove();
         }
-        count += 6;
-        t = current;               // t moves backward
+        return -1;
     }
-    return count;        
 }
