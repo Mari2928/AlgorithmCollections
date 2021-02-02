1f492a4154a8629faddac3b8b5647491e6df8f41
Mari <charmee2928@gmail.com>
2021-02-02 15:52:24 +0900

    Update getMaxInO1.java

--- Stack/getMaxInO1.java
+++ Stack/getMaxInO1.java
@@ -3,26 +3,43 @@
     Input: 1, 6, 3, 4, 5
     st: [(1, 1), (6, 6), (3, 6) (4, 6), (5, 6)]
 */
+import java.util.Stack;
+import java.util.*;
 
-// let each element have max value in the moment
-StackElement {
-  int value;
-  int maxValue;
-}
+public class Test2 {
+	
+    // let each element have max value in the moment
+	class StackElement {
+		int value;
+		int maxValue;
+		StackElement(int v, int maxV){
+			this.value = v;
+			this.maxValue = maxV;
+		}
+	}
+	Stack<StackElement> st = new Stack<>();	
+	
+	void push(int value) {
+		int maxValue = (st.isEmpty() || st.peek().maxValue < value) ? value : st.peek().maxValue;
+		st.push(new StackElement(value, maxValue));
+	}
+	
+	void pop() {
+		st.pop();
+	}
+	
+	int getMax() {
+		return st.peek().maxValue;
+	}
 
-Stack {
-  push(int value) {
-    int maxValue = (st.empty() || st.pop().maxValue < value) ? value : st.pop().maxValue;
-    st.push(new StackElement{value, maxValue});
-  }
-
-  pop() {
-    st.pop();
-  }
-
-  getMax() {
-    return st.pop().maxValue;
-  }
-
-  List<StackElement> st = new List<>();
+	public static void main(String[] args) {
+		Test2 test = new Test2();
+		test.push(1);
+		test.push(5);
+		test.push(3);
+		test.push(4);
+		test.push(6);
+		test.pop();
+		System.out.println(test.getMax());
+	}	
 }
