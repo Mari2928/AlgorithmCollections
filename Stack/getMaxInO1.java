1ba64369feb8245fb416f6af149c0f497b56575e
Mari <charmee2928@gmail.com>
2021-02-02 15:53:38 +0900

    Update getMaxInO1.java

--- Stack/getMaxInO1.java
+++ Stack/getMaxInO1.java
@@ -7,39 +7,39 @@ import java.util.Stack;
 import java.util.*;
 
 public class Test2 {
-	
-    // let each element have max value in the moment
-	class StackElement {
-		int value;
-		int maxValue;
-		StackElement(int v, int maxV){
-			this.value = v;
-			this.maxValue = maxV;
-		}
-	}
-	Stack<StackElement> st = new Stack<>();	
-	
-	void push(int value) {
-		int maxValue = (st.isEmpty() || st.peek().maxValue < value) ? value : st.peek().maxValue;
-		st.push(new StackElement(value, maxValue));
-	}
-	
-	void pop() {
-		st.pop();
-	}
-	
-	int getMax() {
-		return st.peek().maxValue;
-	}
 
-	public static void main(String[] args) {
-		Test2 test = new Test2();
-		test.push(1);
-		test.push(5);
-		test.push(3);
-		test.push(4);
-		test.push(6);
-		test.pop();
-		System.out.println(test.getMax());
-	}	
+    // let each element have max value in the moment
+    class StackElement {
+        int value;
+        int maxValue;
+        StackElement(int v, int maxV){
+            this.value = v;
+            this.maxValue = maxV;
+        }
+    }
+    Stack<StackElement> st = new Stack<>();	
+
+    void push(int value) {
+        int maxValue = (st.isEmpty() || st.peek().maxValue < value) ? value : st.peek().maxValue;
+        st.push(new StackElement(value, maxValue));
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int getMax() {
+        return st.peek().maxValue;
+    }
+
+    public static void main(String[] args) {
+        Test2 test = new Test2();
+        test.push(1);
+        test.push(5);
+        test.push(3);
+        test.push(4);
+        test.push(6);
+        test.pop();
+        System.out.println(test.getMax());
+    }	
 }
